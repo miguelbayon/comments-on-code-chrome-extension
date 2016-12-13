72fddc0d137f55b51987bbbdc7ee0d0b2ca0cdb0
Miguel Bayon <miguel.bayon@iessanandres.com>
2016-12-13 20:52:09 +0100

    La corrección se va copiando sola al portapapeles

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,46 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
-/*	chrome.windows.create({
-		url:"src/browser_action/panel.html",
-		type:"panel",
-		width:300,
-		height:200
-	});	*/
-  document.getElementById('mensaje').addEventListener('keypress', manejarPulsacionEnter);
-  document.getElementById('nueva-correccion').addEventListener('click', botonNuevaCorreccionPulsado);
-  document.getElementById("mensaje").focus();
-  chrome.storage.local.get('correccion', function (contenidoLocalStorage) {
-	$('#correccion-hasta-ahora').html(contenidoLocalStorage.correccion);
-  });  
+	document.getElementById('mensaje').addEventListener('keypress', manejarPulsacionEnter);
+	document.getElementById('nueva-correccion').addEventListener('click', botonNuevaCorreccionPulsado);
+	document.getElementById("mensaje").focus();
+	chrome.storage.local.get('correccion', function (contenidoLocalStorage) {
+		$('#correccion-hasta-ahora').html(contenidoLocalStorage.correccion);
+	});  
+
+	document.addEventListener('copy', function(e) {
+		e.clipboardData.setData('text/html', $('#correccion-hasta-ahora').html());
+		e.preventDefault();
+
+	});
 });	
 	
 	
 function manejarPulsacionEnter(e) {
-    if (e.keyCode == 13 || event.which == 13) {
-        event.preventDefault();      
-        
-        var correccion = "";
-        chrome.storage.local.get('correccion', function (contenidoLocalStorage) {
+	if (e.keyCode == 13 || event.which == 13) {
+		event.preventDefault();      
+
+		var correccion = "";
+		chrome.storage.local.get('correccion', function (contenidoLocalStorage) {
 			correccion += contenidoLocalStorage.correccion;
 			chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 				
-				correccion += '<p>* [' + $('#mensaje').val() + '](' + tabs[0].url + ')</p>';
+				if (correccion != "") {
+					correccion += '<br>';
+				}
+				correccion += '* [' + $('#mensaje').val() + '](' + tabs[0].url + ')';
 				chrome.storage.local.set({ "correccion": correccion }, function(){
 					$('#mensaje').val("");
 				});			
-				$('#correccion-hasta-ahora').html(correccion);				
+				$('#correccion-hasta-ahora').html(correccion);
+				document.execCommand('copy');				
 			});
 
-        });         
+		});         
 
-          
-        return false;
-    }
+
+		return false;
+	}
 }
 
 function botonNuevaCorreccionPulsado(e) {
 	chrome.storage.local.set({ "correccion": "" }, function(){
-	  chrome.storage.local.get('correccion', function (contenidoLocalStorage) {
-		$('#correccion-hasta-ahora').html(contenidoLocalStorage.correccion);
-	  });		
-	});		
+		chrome.storage.local.get('correccion', function (contenidoLocalStorage) {
+			$('#correccion-hasta-ahora').html(contenidoLocalStorage.correccion);
+		});		
+	});	
+	$('#mensaje').focus();	
 }
+
+
+
