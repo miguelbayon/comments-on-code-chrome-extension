27798a1f67aae374f3a75a1b79f039b3b014046e
Miguel Bayon <miguel.bayon@iessanandres.com>
2017-12-10 20:17:24 +0100

    Queda copiada la correccion actual en el portapapeles

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 	document.getElementById('mensaje').addEventListener('keypress', manejarPulsacionEnter);
-	document.getElementById('nueva-correccion').addEventListener('click', botonNuevaCorreccionPulsado);
+	document.getElementById('nueva-correccion').addEventListener('click', botonNuevaCorreccionPulsado);   
 	document.getElementById("mensaje").focus();
 	chrome.storage.local.get('correccion', function (contenidoLocalStorage) {
 		$('#correccion-hasta-ahora').html(contenidoLocalStorage.correccion);
@@ -16,27 +16,42 @@ document.addEventListener('DOMContentLoaded', function () {
 	
 function manejarPulsacionEnter(e) {
 	if (e.keyCode == 13 || event.which == 13) {
-		event.preventDefault();      
-
+		event.preventDefault();    
+        
+        var modoMarkdown = false;
 		var correccion = "";
+        
 		chrome.storage.local.get('correccion', function (contenidoLocalStorage) {
 			correccion += contenidoLocalStorage.correccion;
 			chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 				
-				if (correccion != "") {
-					correccion += '<br>';
-				}
-				correccion += '* [' + $('#mensaje').val() + '](' + tabs[0].url + ')';
+				if (modoMarkdown) {                
+                    if (correccion != "") {
+                        correccion += '<br>';
+                    }
+                }
+                else {
+                    if (correccion != "") {
+                        correccion += '<br>';
+                    }                    
+                }
+				
+				if (modoMarkdown) {
+                    correccion += '* [' + $('#mensaje').val() + '](' + tabs[0].url + ')';                    
+                }
+                else {
+                    correccion += tabs[0].url + ": " + $('#mensaje').val() + ".";     
+                }
+                
 				chrome.storage.local.set({ "correccion": correccion }, function(){
 					$('#mensaje').val("");
 				});			
 				$('#correccion-hasta-ahora').html(correccion);
-				document.execCommand('copy');				
+                copyTextToClipboard(correccion);   				
 			});
 
 		});         
-
-
+        
 		return false;
 	}
 }
@@ -51,4 +66,17 @@ function botonNuevaCorreccionPulsado(e) {
 }
 
 
+function copyTextToClipboard(text) {
+        
+    chrome.tabs.executeScript({code: "\
+        var input = document.createElement('textarea');\
+        input.value = '" + text.split('<br>').join('\\n') + "';\
+        input.style.cssText = 'opacity:0; position:fixed';\
+        document.body.appendChild(input);\
+        input.focus();\
+        input.select();\
+        document.execCommand('Copy');\
+        input.remove();\
+    "});
+}
 
